a5ebaefe5e021ac56aa838e19150df448aba4ee8
AldairCruz501 <acruz.pbxhosting@gmail.com>
2025-03-06 13:01:29 -0600

    Axios

---
+++ 01-bases/src/bases/10-axios.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+import { GIFResponse } from "../interfaces/gif.response";
+
+const apiKey = 'bXYm2JTD8V90CtxPdv3CsiVet5rpb9Hz';
+
+export const giphyApi = axios.create({
+    baseURL: 'https://api.giphy.com/v1/gifs/',
+    params: {
+        api_key: apiKey,
+    }
+});
+
+//export default giphyApi;
+
+giphyApi.get<GIFResponse>('/random')
+    .then( resp => console.log( resp.data.data.images.downsized_large.url ))
+    .catch( err => console.log( err ) );
