fb467cf7c328df621cfcd4320e59becb9087212c
Aldair Cruz Bautista <aldair.cruzbautista@outlook.com>
2025-03-05 22:09:18 -0600

    Fetch API

---
+++ 01-bases/src/bases/09-fetch-api.ts
@@ -0,0 +1,22 @@
+
+const apiKey = 'bXYm2JTD8V90CtxPdv3CsiVet5rpb9Hz';
+
+/*fetch(`https://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`)
+    .then(resp => {
+        return resp.json()
+    })
+    .then(body => console.log({ body }))
+    .catch(err => console.log( err ))*/
+
+/*fetch(`https://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`)
+    .then(resp => resp.json())
+    .then(body => console.log({ body }))
+    .catch(err => console.log( err ));*/
+
+fetch(`https://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`)
+    .then(resp => resp.json())
+    .then(body => {
+        //console.log(body.data.type)
+        console.log(body.data.images.downsized_still.url)
+    })
+    .catch(err => console.log( err ));
