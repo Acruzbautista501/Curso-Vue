81cb1374a3f24fce31e00fb2ec925d7c03179ab0
Aldair Cruz Bautista <aldair.cruzbautista@outlook.com>
2025-03-17 14:41:07 -0600

    Eventos y propiedaded computadas

--- 02-intro-vue/app.js
+++ 02-intro-vue/app.js
@@ -1,4 +1,4 @@
-const quotes = [
+const originalQuotes = [
     { quote: 'The night is darkest just before the dawn. And I promise you, the dawn is coming.', author: 'Harvey Dent, The Dark Knight' },
     { quote: 'I believe what doesn’t kill you simply makes you, stranger.', author: 'The Joker, The Dark Knight' },
     { quote: 'Your anger gives you great power. But if you let it, it will destroy you… As it almost did me', author: 'Henri Ducard, Batman Begins' },
@@ -7,21 +7,33 @@ const quotes = [
     { quote: 'Yes, father. I shall become a bat.', author: 'Bruce Wayne/Batman, Batman: Year One' },
 ];
 
-const {createApp, ref} = Vue
+const {createApp, ref, computed} = Vue
 
 const app = createApp ( {
 
     setup() {
 
         const showAuthor = ref(true) // Propiedad reactiva que va a manejar el valor de author
+        const quotes = ref(originalQuotes) // Propiedad reactiva que manda a llamar al arreglo para mostrar su información
+        const totalQuotes = computed( ()=> {
+            return quotes.value.length; // Función que retorna el valor esperado del arreglo
+        }); // Propiedad computada para mostrar el total de frases que tiene el arreglo
+
         const toggleAuthor = () => {
             showAuthor.value = !showAuthor.value
+        }
+
+        const addQuote = () => {
+            quotes.value.unshift({ quote: 'Hola Mundo', author: 'Aldair Cruz'}); // .value sirve para referenciar al valor 
+            console.log(quotes)
         }
         
         return {
             quotes,
             showAuthor,
-            toggleAuthor
+            toggleAuthor,
+            addQuote,
+            totalQuotes
         }
     }
 
