3b0fb216bb229f088145dcc1695a99b86df7f0c0
Aldair Cruz Bautista <aldair.cruzbautista@outlook.com>
2025-03-17 18:01:28 -0600

    Mi primer componente en Vue.js

--- 03-indecision-app/src/App.vue
+++ 03-indecision-app/src/App.vue
@@ -1,34 +1,16 @@
 <!--El template representa la plantilla que contiene el contenido HTML del componente-->
 <template>
-    <section>
-        <h3>Counter: {{ counter }}</h3>
-        <h3>Square: {{ squareCounter }}</h3>
-    </section>
+    <h1>Mi primera aplicación de Vue.js</h1>
+    <hr>
 
-    <div>
-        <button @click="counterAdd">+1</button> <!--Función para lanazar el evento de incrementar-->
-        <button @click="counterRest">-1</button> <!--Función para lanazar el evento de decrementar-->
-    </div>
+    <MyCounter/>
 </template>
 
 <!--La etiqueta script contendrá todo el JavaScript de Vue relacionado con el componente.
 Se tiene que poner lang="ts" para poder trabajar con TypScript y setup para trabajar con el 
 Composition API-->
 <script lang="ts" setup>
-import { computed, ref } from 'vue';
-
-    const counter = ref(4); /*Vue mantiene el estado de sus variables, siendo bastante útil, 
-    porque permite hacer cambios sin necesidad de volver a recargar la aplicación*/
-    
-    const squareCounter = computed(() => counter.value * counter.value)  //Propiedad computada para mostrar el total del puntaje
-
-    const counterAdd = () => {
-        counter.value++
-    }
-
-    const counterRest = () => {
-        counter.value--
-    }
+import MyCounter from './components/MyCounter.vue';
 
 
 </script>
